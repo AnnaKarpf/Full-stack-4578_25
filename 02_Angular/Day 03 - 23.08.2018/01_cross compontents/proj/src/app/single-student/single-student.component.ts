8b649ce75398c39cc1ef1eeb09880bc88884a1f8
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-08-23 21:41:31 +0300

    fullstack

--- 02_Angular/Day 03 - 23.08.2018/01_cross compontents/proj/src/app/single-student/single-student.component.ts
+++ 02_Angular/Day 03 - 23.08.2018/01_cross compontents/proj/src/app/single-student/single-student.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 @Component({
   selector: "app-single-student",
   templateUrl: "./single-student.component.html",
-  styleUrls: ["./single-student.component.css"]
+  styleUrls: []
 })
 export class SingleStudentComponent {
   @Input() studentName: string;
